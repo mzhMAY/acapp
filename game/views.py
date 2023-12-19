18977f20a7a4cf9bf8d1e04fd0f2744ee6c613d1
mzhMAY <mazhonghuiMAY@163.com>
2023-12-19 10:47:56 +0800

    write my first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(resquest):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line2 = '<img src="https://cdn.acwing.com/media/article/image/2023/03/17/1_067a3e9cc4-background.gif" width=2000>'
+    return HttpResponse(line1 + line2)
